48f05095ffa8e4663310fb03bf3e000f17af5732
Muhammadomer902 <sayyidmuhammadomer@gmail.com>
2025-10-04 22:38:15 +0500

    Fixed Menu Option Issue

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -10,7 +10,7 @@ export default function About() {
         <IntroductionBox />
         <OriginationBox />
         <ImpactWanted />
-        <OurFoundation />
+        {/* <OurFoundation /> */}
     </div>
   );
 }
