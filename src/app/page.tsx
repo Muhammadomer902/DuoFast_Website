919f89c6bb9eea5d45cd77dab4e89f249b44503a
Muhammadomer902 <sayyidmuhammadomer@gmail.com>
2025-10-04 20:04:51 +0500

    Fixed Menu Option Issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
     <div>
       <IntroductionBox />
       <StandOutBox />
-      {/* <AboutUs /> */}
+      <AboutUs />
       <WhatWeDo />
       <BuildDreamProject />
       <WhyXenotric />
